9b632ddbd5e353e0c081d3d69042240cebf03ebf
max <161075989+maxc0des@users.noreply.github.com>
2024-07-21 21:55:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 console.log('hello world')
+let current_img = 1;
+const images = 4;
 document.addEventListener('DOMContentLoaded', function() {
     fetch('latest-project.txt')
         .then(response => response.text())
@@ -7,3 +9,16 @@ document.addEventListener('DOMContentLoaded', function() {
         })
         .catch(error => console.error('Error loading the text file:', error));
 });
+document.getElementById('image_frame').addEventListener("click", change_image);
+
+function change_image() {
+    if(current_img+1>images){
+        next_img = 1;
+    }
+    else{
+        next_img = current_img + 1;
+    }
+    let source = "/images/image"+ next_img + ".jpg";
+    current_img = next_img;
+    document.getElementById("image_frame").src = source;
+}
